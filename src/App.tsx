30b92563968c738e2529a2f8468842ff63c64b8e
Vyary <vyaryw@gmail.com>
2026-01-23 03:02:11 +0200

    add pass-through + shortcut toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { createSignal, onMount } from "solid-js";
 import "./App.css";
+import usePassthroughShortcut from "./hooks/usePassthroughShortcut";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+  const [passthrough, setPassthrough] = createSignal(false);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
+  onMount(() => usePassthroughShortcut(passthrough, setPassthrough));
 
   return (
     <main class="bg-transparent">
