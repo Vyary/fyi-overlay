83d4880d8dad0615a4843004cb430fd789102a05
Vyary <vyaryw@gmail.com>
2026-01-24 03:46:43 +0200

    enable fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import "./App.css";
 import usePassthroughShortcut from "./hooks/usePassthroughShortcut";
 import FileSelect from "./components/FileSelect";
 import FileTail from "./components/FileTail";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 function App() {
   const [passthrough, setPassthrough] = createSignal(false);
   const [filePath, setFilePath] = createSignal("");
   const [zone, setZone] = createSignal("");
 
-  onMount(() => usePassthroughShortcut(passthrough, setPassthrough));
+  onMount(() => {
+    getCurrentWindow().maximize();
+    usePassthroughShortcut(passthrough, setPassthrough);
+  });
 
   return (
     <main class="bg-transparent">
